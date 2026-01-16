59e0c62945610689e4350275d94781cf498f9d41
TheDarkKn1ght-hub <marvin.stark@web.de>
2026-01-16 10:41:48 +0100

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,17 @@
+document.getElementById("attendanceForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const data = new FormData(e.target);
+
+  const body =
+    "Antwort: " + data.get("response") + "%0D%0A%0D%0A" +
+    "Name: " + data.get("name") + "%0D%0A" +
+    "Telefon: " + data.get("phone") + "%0D%0A%0D%0A" +
+    "Weitere Personen:%0D%0A" + data.get("guests") + "%0D%0A%0D%0A" +
+    "Nachricht:%0D%0A" + data.get("message");
+
+  window.location.href =
+    "mailto:EUREMAIL@BEISPIEL.DE" +
+    "?subject=Zu- oder Absage Hochzeit" +
+    "&body=" + body;
+});
